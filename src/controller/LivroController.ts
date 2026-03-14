b042c3f517c8cd4a02e8bb227007ee94608ffa56
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-03-14 15:20:45 -0300

    feat: criando método para listar livro por ID

--- src/controller/LivroController.ts
+++ src/controller/LivroController.ts
@@ -11,6 +11,19 @@ class LivroController extends Livro {
             return res.status(500).json({ mensagem: "Erro ao recuperar as informações dos livros." });
         }
     }
+
+    static async livro(req: Request, res: Response) {
+        try {
+            const idLivro = parseInt(req.params.id as string);
+
+            const livro = await Livro.listarLivro(idLivro);
+            res.status(200).json(livro);
+        } catch (error) {
+            console.log(`Erro ao acessar método herdado: ${error}`);    // Exibe erros da consulta no console
+            res.status(500).json("Erro ao recuperar as informações do livro.");  // Retorna mensagem de erro com status code 400
+        }
+    }
+
 }
 
 export default LivroController;
